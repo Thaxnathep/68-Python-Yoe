d4bcea3756b0d51b43fe2f33f90968d6eb18a4cb
Thaxnathep <s6806022510343@email.kmutnb.ac.at>
2025-06-26 16:37:22 +0700

    FirstCommit

---
+++ Lecture-01/Testprint.py
@@ -0,0 +1,9 @@
+#print('Hello World')
+#print('Thanathep Preammanee')
+#print("I'am hungry")
+#print("I'amgood and \"I will do my best\"")
+print('/\\')
+print('\ta\na\ta\ta\n\ta')
+print('''\ta
+a\ta\ta
+\ta''')
