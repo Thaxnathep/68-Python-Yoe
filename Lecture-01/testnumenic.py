b2a59363e7534d89597be13585f560637bb2e135
Thaxnathep <s6806022510343@email.kmutnb.ac.at>
2025-06-26 17:02:23 +0700

    End Lecture-01

---
+++ Lecture-01/testnumenic.py
@@ -0,0 +1,10 @@
+from math import pi
+
+#print('%d' %100)
+#print('%d' %100.55)
+#print('%f' %100.559)
+print('%.2f' %100.555)
+print('%.6f' %pi)
+print('My age is %d. I have %.2f Bath.' %(21,5020.55))
+print('My age is ' + str(25) + ' I have ' + str(5000.50) + 'Bath.')
+print('5+4 =' ,5+4)
