1413f7b3a4137bf58c7c6177718de564723d559c
Thaxnathep <s6806022510343@email.kmutnb.ac.at>
2025-07-03 16:01:05 +0700

    End Lecture 2

---
+++ Lecture-02/Test_Converse.py
@@ -0,0 +1,21 @@
+age = 25
+hight = float(age)
+print(hight)
+
+height = 5.6
+age = int(height)
+print(age)
+
+num_str = "123"
+num = int(num_str)
+print(num)
+
+x = 15
+y = 4
+print('x + y =',x+y)
+print('x - y =',x-y)
+print('x * y =',x*y)
+print('x / y =',x/y)
+print('x % y =',x%y)
+print('x // y =',x//y)
+print('x ** y =',x**y)
